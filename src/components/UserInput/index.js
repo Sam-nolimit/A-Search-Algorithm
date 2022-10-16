8296826a15728a56d702c23eaf55130294bd1413
Temitope Adejolu <tadejolu@gmail.com>
2022-10-16 12:14:46 +0100

    A star search algorithm visual implementation

---
+++ src/components/UserInput/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css'
+export default function UserInput({ startNode, endNode, setStartNode, setEndNode }) {
+	return (
+		<div className="user-input-container">
+			<div className="user-input-wrapper">
+				<div className="user-text-input"  >
+					<p className="user-input-label">Start: </p>
+					<input className="user-input" type="text" value={startNode} onChange={(event) => setStartNode(event.target.value)} />
+				</div>
+				<div className="user-text-input" >
+					<p className="user-input-label">Goal: </p>
+					<input className="user-input" type="text" value={endNode} onChange={(event) => setEndNode(event.target.value)} />
+				</div>
+			</div>
+		</div>
+	)
+} 
